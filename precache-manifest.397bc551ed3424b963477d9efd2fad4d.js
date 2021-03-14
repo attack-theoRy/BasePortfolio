9f3e4380dde8aca12b9d24f98cefaac6a6d9d83c
Pirooz Wallace <PiroozWallace@outlook.com>
2021-03-13 20:36:59 -0800

    Updates

---
+++ precache-manifest.397bc551ed3424b963477d9efd2fad4d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "402ef47739033b4214d893c1a0f6707a",
+    "url": "/BasePortfolio/index.html"
+  },
+  {
+    "revision": "c7d7b61af91d02a66704",
+    "url": "/BasePortfolio/static/css/2.2b31962b.chunk.css"
+  },
+  {
+    "revision": "2f6feadf21e17828f435",
+    "url": "/BasePortfolio/static/css/main.6f84afec.chunk.css"
+  },
+  {
+    "revision": "c7d7b61af91d02a66704",
+    "url": "/BasePortfolio/static/js/2.4797c88d.chunk.js"
+  },
+  {
+    "revision": "2f6feadf21e17828f435",
+    "url": "/BasePortfolio/static/js/main.1c2b461d.chunk.js"
+  },
+  {
+    "revision": "4a29f12efae67f9d2258",
+    "url": "/BasePortfolio/static/js/runtime~main.38297aab.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/BasePortfolio/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/BasePortfolio/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/BasePortfolio/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/BasePortfolio/static/media/slick.f97e3bbf.svg"
+  }
+]);
